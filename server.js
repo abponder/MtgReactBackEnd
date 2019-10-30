ae0e01fb1715b60481098ce852a8c87c4ae8f0a4
alexbponder@gmail.com <alexbponder@gmail.com>
2019-10-29 18:29:08 -0700

    finished for now

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const port = 8080
 const cors = require('cors');
+app.use(express.json()); 
 
 
 app.use(cors());
@@ -27,16 +28,29 @@ app.get("/api/testing", (req,res)=> {
   res.send('testing')
 })
 
-app.post('/api/edit', (req,res)=> {
-
-  connection.query(`insert into meetings (meeting_title, start_date, start_time, attendees, topics_discussed, status)
-  values ('test meeting3',  '2019-06-03', '10:00:00', 'Mila3, Alex3', 'MySql3 and React3','open')`, (err, result) => {
-    if (err) console.log(err)
-      console.log('its working !', result)
-    
+app.put('/api/edit', (req,res)=> {
+  console.log('this is the data coming back', req.body)
+  //changed to STR_TO_DATE, added ticks, put comma in! argh
+  connection.query(`
+    UPDATE meetings 
+    SET meeting_title = '${req.body.meetingTitle}',  
+    start_date = STR_TO_DATE('${req.body.startDate}', "%M %D, %Y"), 
+    start_time = '${req.body.startTime}',
+    attendees = '${req.body.attendees}',
+    topics_discussed = '${req.body.topicsDiscussed}',
+    status = '${req.body.status}'
+    WHERE meeting_id = '${req.body.meetingId}'`, (err, result) => {
+      console.log('err :',err)  
+    console.log('new edit data', result)
   })
+  // connection.query(`insert into meetings (meeting_title, start_date, start_time, attendees, topics_discussed, status)
+  // values ('${req.body.meetingTitle}',  '${req.body.startDate}', '${req.body.startTime}', '${req.body.attendees}', '${req.body.topicsDiscussed}','${req.body.status}')`, (err, result) => {
+  //   if (err) console.log(err)
+  //     console.log('its working !', result)
+    
+  // })
 
-  res.send()
+  res.send(req.body)
 
 })
 
@@ -45,7 +59,19 @@ app.get('/api/schedule', (req,res)=> {
   connection.query(`SELECT  meeting_id, meeting_title, DATE_FORMAT(start_date, "%M %D, %Y"), start_time, attendees, topics_discussed, status FROM meetings`, (err, rows, fields) => {
     if (err) console.log(err)
       console.log('its working !', rows)
-    res.send(rows)
+      let formatedrows=rows.map(record => {
+        return {
+          meetingId:record.meeting_id,
+          meetingTitle:record.meeting_title,
+          startDate:record['DATE_FORMAT(start_date, "%M %D, %Y")'],
+          startTime:record.start_time,
+          attendees:record.attendees,
+          topicsDiscussed:record.topics_discussed,
+          status:record.status[0].toUpperCase() + record.status.slice(1)
+
+        }
+      })
+    res.send(formatedrows)
     // results = rows
   })
 
